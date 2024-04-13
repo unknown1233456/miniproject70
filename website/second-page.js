ac9eb4931b707ace064315f182325a113a4a7283
unknown <84895074+unknown1233456@users.noreply.github.com>
2024-04-13 13:13:47 +0530

    Add files via upload

---
+++ website/second-page.js
@@ -0,0 +1,58 @@
+let next = document.querySelector('.next')
+let prev = document.querySelector('.prev')
+
+next.addEventListener('click', function(){
+    let items = document.querySelectorAll('.item')
+    document.querySelector('.slide').appendChild(items[0])
+})
+
+prev.addEventListener('click', function(){
+    let items = document.querySelectorAll('.item')
+    document.querySelector('.slide').prepend(items[items.length - 1]) // here the length of items = 6
+})
+
+function windoGame(){
+    window.open('game-page.html',"name","toobar=0,locationbar=0,width=1035px ,height=600px,left=500px,top=200px;");
+}
+function windopro(){
+    window.open('poductivity.html',"name","toobar=0,locationbar=0,width=1035px ,height=600px,left=500px,top=200px;");
+}
+function windoChat(){
+  window.open('chat/chat.html',"name","toobar=0,locationbar=0,width=1035px ,height=700px,left=500px,top=200px;");
+}
+const openModalButtons = document.querySelectorAll('[data-modal-target]')
+const closeModalButtons = document.querySelectorAll('[data-close-button]')
+const overlay = document.getElementById('overlay')
+
+openModalButtons.forEach(button => {
+  button.addEventListener('click', () => {
+    const modal = document.querySelector(button.dataset.modalTarget)
+    openModal(modal)
+  })
+})
+
+overlay.addEventListener('click', () => {
+  const modals = document.querySelectorAll('.modal.active')
+  modals.forEach(modal => {
+    closeModal(modal)
+  })
+})
+
+closeModalButtons.forEach(button => {
+  button.addEventListener('click', () => {
+    const modal = button.closest('.modal')
+    closeModal(modal)
+  })
+})
+
+function openModal(modal) {
+  if (modal == null) return
+  modal.classList.add('active')
+  overlay.classList.add('active')
+}
+
+function closeModal(modal) {
+  if (modal == null) return
+  modal.classList.remove('active')
+  overlay.classList.remove('active')
+}
